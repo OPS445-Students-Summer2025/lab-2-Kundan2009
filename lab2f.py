1249231a8e363d1edf744b68cd9b57c0cb0b48d9
Kundan Patel <kppatel43@myseneca.ca>
2025-05-22 23:46:28 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+#Author: Kundan Patel
+#Author ID: 157024225
+#Date created: 2025/05/22
+
+import sys
+
+timer =int(sys.argv[1])
+
+while timer!=0:
+    print(timer)
+    timer=timer-1
+
+print('blast off!')
