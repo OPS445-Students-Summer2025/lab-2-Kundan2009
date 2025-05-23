7361af8fd1558a143906c1757d744aeccd9884d1
Kundan Patel <kppatel43@myseneca.ca>
2025-05-22 23:47:15 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#Author: Kundan Patel
+#Author ID: 157024225
+#Date created: 2025/05/22
+
+timer =10
+
+while timer!=0:
+    print(timer)
+    timer=timer-1
+
+print('blast off!')
